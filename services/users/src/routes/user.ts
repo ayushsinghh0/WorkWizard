e359a908b3c2a2070e3e05934ed33e410ee4db2c
ayushsinghh0 <ayushraj4820@gmail.com>
2026-02-09 13:47:53 +0530

    update profice and update profile pic userpoint completed

--- services/users/src/routes/user.ts
+++ services/users/src/routes/user.ts
@@ -1,10 +1,16 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
-import { myProfile } from "../controllers/user.js";
+import { getUserProfile, myProfile, updateProfilePic, updateUserProfile } from "../controllers/user.js";
+import uploadFile from "../middleware/multer.js";
 
 const router=express.Router();
 
 router.get("/me",isAuth,myProfile);
+router.get("/:userId",isAuth,getUserProfile);
+router.put("/update/profile",isAuth,updateUserProfile);
+router.put("/update/pic",isAuth,uploadFile,updateProfilePic);
+
+
 
 export default router;
 
